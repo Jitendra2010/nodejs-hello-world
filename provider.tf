a9002fdb9ed8eb1b98ba85eab60cabf0e5423924
Jitendra2010 <117669041+Jitendra2010@users.noreply.github.com>
2025-02-07 15:50:52 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+  
+  subscription_id = "075af542-f71c-4113-a260-46a5d627bcf9"
+  tenant_id       = "52f25147-2265-4d27-bf44-c5215cd1350e"
+  # client_id       = "your-client-id"
+  # client_secret   = "your-client-secret"
+}
